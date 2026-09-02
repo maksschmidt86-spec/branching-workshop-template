9f98b35fb077c26c543114be09ca2094607e59f1
Egbert Brenldle <egbertbrendle@web.de>
2026-09-02 12:45:47 +0200

    Comment out volume conversion in main function
    Comment out the volume conversion print statement.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from length_converter import meters_to_kilometers
 from weight_converter import grams_to_kilograms
 from temperature_converter import celsius_to_fahrenheit
-from volume_converter import liters_to_milliliters
+#from volume_converter import liters_to_milliliters
 from area_converter import square_meters_to_square_kilometers
 from speed_converter import mps_to_kph
 from time_converter import seconds_to_minutes
@@ -11,7 +11,7 @@ def main():
     print("Length: 5000 meters = ", meters_to_kilometers(5000), "kilometers")
     print("Weight: 10000 grams = ", grams_to_kilograms(10000), "kilograms")
     print("Temperature: 37°C = ", celsius_to_fahrenheit(37), "°F")
-    print("Volume: 3 liters = ", liters_to_milliliters(3), "milliliters")
+#    print("Volume: 3 liters = ", liters_to_milliliters(3), "milliliters")
     print("Area: 1000000 square meters = ", square_meters_to_square_kilometers(1000000), "square kilometers")
     print("Speed: 10 m/s = ", mps_to_kph(10), "km/h")
     print("Time: 120 seconds = ", seconds_to_minutes(120), "minutes")
